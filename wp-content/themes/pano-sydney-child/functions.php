04aaa35391b2650611534030bd75681148dda26f
Mark Foster <foster154@gmail.com>
2017-10-24 20:59:20 -0600

    remove ignored files

--- wp-content/themes/pano-sydney-child/functions.php
+++ wp-content/themes/pano-sydney-child/functions.php
@@ -55,3 +55,80 @@ function our_team_function() {
 }
 
 add_shortcode('our_team', 'our_team_function');
+
+// "Product Features" shortcode for the Pricing page
+function pano_product_features_function() {
+
+	ob_start();
+	?>
+	
+	<div class="product-list row">
+
+		<?php if ($r->have_posts()) : while ( $r->have_posts() ) : $r->the_post(); 
+			
+		?>
+		<div class="row">
+			<div class="product">
+				<div class="product-pic col-sm-2 col-sm-offset-0 col-xs-4 col-xs-offset-4">
+					<?php the_post_thumbnail('sydney-medium-thumb'); ?>
+				</div>
+				<div class="product-info col-sm-10 col-xs-12">
+					<div class="product-name"><?php the_title(); ?></div>
+					<div class="product-desc"><?php the_content(); ?></div>
+				</div>
+			</div>
+		</div><!-- /.row -->
+
+		<?php endwhile; endif; ?>
+	</div><!-- /.row -->
+
+<?php
+
+	return ob_get_clean();
+
+}
+
+add_shortcode('product-features', 'pano_product_features_function');
+
+// "Products" shortcode for the Pricing page
+function pano_products_function() {
+  
+	$r = new WP_Query(array(
+	'no_found_rows'       => true,
+	'post_status'         => 'publish',
+	'post_type' 		  => 'product',
+	'orderby'			  => 'meta_value',
+	'meta_key'			  => 'wpcf-order',
+	'order'				  => 'ASC'
+	) );
+
+	ob_start();
+	?>
+	
+	<div class="product-list row">
+
+		<?php if ($r->have_posts()) : while ( $r->have_posts() ) : $r->the_post(); 
+			
+		?>
+		<div class="row">
+			<div class="product">
+				<div class="product-pic col-sm-2 col-sm-offset-0 col-xs-4 col-xs-offset-4">
+					<?php the_post_thumbnail('sydney-medium-thumb'); ?>
+				</div>
+				<div class="product-info col-sm-10 col-xs-12">
+					<div class="product-name"><?php the_title(); ?></div>
+					<div class="product-desc"><?php the_content(); ?></div>
+				</div>
+			</div>
+		</div><!-- /.row -->
+
+		<?php endwhile; endif; ?>
+	</div><!-- /.row -->
+
+<?php
+
+	return ob_get_clean();
+
+}
+
+add_shortcode('products', 'pano_products_function');
